1796ea1aaaba50c364a4449c44d5600b31f986b2
ribhar <ribharscode@gmail.com>
2023-07-31 14:22:38 +0530

    add sorting testcases

--- server/tests/controllers/search.controller.test.ts
+++ server/tests/controllers/search.controller.test.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from 'express';
 import { searchPosts } from '../../src/controllers/search.controller';
 
+
+
+interface Post {
+    name: string;
+    image: string;
+    description: string;
+    dateLastEdited: string;
+}
+
 // Mock the data module
 jest.mock('../../src/data.json', () => [
   {
@@ -11,6 +20,19 @@ jest.mock('../../src/data.json', () => [
   },
   // Add more mock data if needed
 ]);
+
+
+// Mock the data module
+const data = [
+    {
+      name: 'Post 1',
+      image: 'image1.jpg',
+      description: 'This is the first post',
+      dateLastEdited: '2023-07-30',
+    },
+    // Add more mock data if needed
+];
+  
 
 // Define the mockRequest function
 const mockRequest = (overrides?: any): Request => {
@@ -28,40 +50,123 @@ const mockResponse = (): Response => {
 };
 
 describe('searchPosts function', () => {
-  it('should return filtered and paginated results when a valid search query is provided', () => {
-    const req = mockRequest({
-      query: {
-        query: 'post',
-        sortBy: 'name',
-        page: 1,
-        limit: 1,
-      },
-    });
-    const res = mockResponse();
-
-    // Call the searchPosts function
-    searchPosts(req, res);
-
-    // Expect that the response JSON is called with the correct data
-    expect(res.json).toHaveBeenCalledWith({
-      httpStatus: 200,
-      body: {
-        success: true,
-        msg: 'Result successfully fetched',
-        data: {
-          results: [
-            {
-              name: 'Post 1',
-              image: 'image1.jpg',
-              description: 'This is the first post',
-              dateLastEdited: '2023-07-30',
+    it('should return all data when no search query provided', () => {
+        const req = mockRequest(); // No search query provided
+        const res = mockResponse();
+    
+        // Call the searchPosts function
+        searchPosts(req, res);
+    
+        // Expect that the response JSON is called with all the data
+        expect(res.json).toHaveBeenCalledWith({
+          httpStatus: 200,
+          body: {
+            success: true,
+            msg: 'Result successfully fetched',
+            data: {
+              results: [
+                {
+                  name: 'Post 1',
+                  image: 'image1.jpg',
+                  description: 'This is the first post',
+                  dateLastEdited: '2023-07-30',
+                },
+              ],
+              totalPages: 1,
             },
-          ],
-          totalPages: 1,
-        },
-      },
+          },
+        });
     });
-  });
 
-  // Add more test cases here...
+    it('should return all data when invalid page and limit values provided', () => {
+        const req = mockRequest({
+          query: {
+            page: 1,
+            limit: 10,
+          },
+        });
+        const res = mockResponse();
+      
+        // Call the searchPosts function
+        searchPosts(req, res);
+      
+        // Expect that the response JSON is called with all the data
+        expect(res.json).toHaveBeenCalledWith({
+          httpStatus: 200,
+          body: {
+            success: true,
+            msg: 'Result successfully fetched',
+            data: {
+              results: data, // Since invalid page and limit, return all data
+              totalPages: 1,
+            },
+          },
+        });
+    });
+    
+    it('should return data sorted by dateLastEdited in descending order', () => {
+        const req = mockRequest({
+          query: {
+            sortBy: 'dateLastEdited',
+          },
+        });
+        const res = mockResponse();
+      
+        // Call the searchPosts function
+        searchPosts(req, res);
+      
+        // Expect that the response JSON is called with the data sorted by dateLastEdited
+        const sortedData = [...data].sort(
+          (a: Post, b: Post) => new Date(b.dateLastEdited).getTime() - new Date(a.dateLastEdited).getTime()
+        );
+        expect(res.json).toHaveBeenCalledWith({
+          httpStatus: 200,
+          body: {
+            success: true,
+            msg: 'Result successfully fetched',
+            data: {
+              results: sortedData,
+              totalPages: 1,
+            },
+          },
+        });
+      });
+      
+
+    it('should return filtered and paginated results when a valid search query is provided', () => {
+        const req = mockRequest({
+        query: {
+            query: 'post',
+            sortBy: 'name',
+            page: 1,
+            limit: 1,
+        },
+        });
+        const res = mockResponse();
+
+        // Call the searchPosts function
+        searchPosts(req, res);
+
+        // Expect that the response JSON is called with the correct data
+        expect(res.json).toHaveBeenCalledWith({
+        httpStatus: 200,
+        body: {
+            success: true,
+            msg: 'Result successfully fetched',
+            data: {
+            results: [
+                {
+                name: 'Post 1',
+                image: 'image1.jpg',
+                description: 'This is the first post',
+                dateLastEdited: '2023-07-30',
+                },
+            ],
+            totalPages: 1,
+            },
+        },
+        });
+    });
+
+    
 });
